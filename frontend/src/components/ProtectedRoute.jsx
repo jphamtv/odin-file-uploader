8bf8e365fb82ee1eec63b397c02fd20dd1ab22ca
J <jpham26@gmail.com>
2024-12-04 17:51:25 -0800

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+// src/components/ProtectedRoutes.jsx
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useAuth } from '../hooks/useAuthContext';
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />
+  }
+
+  return children;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProtectedRoute;
